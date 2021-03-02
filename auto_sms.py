c35ffa668804165cdb5d801f792ee95f1c56e3da
felix <felix.weidenholzer@outlook.com>
2021-03-02 18:53:17 +0100

    Revert "cull the weak"
    this deculls the weak

--- auto_sms.py
+++ auto_sms.py
@@ -1,5 +1,27 @@
 import serial
 import time
+import re
+
+p = re.compile("READY")
+
+nl = b"\n"
+
+port = serial.Serial("/dev/serial0", baudrate=115000, timeout=3)
+
+port.write(b"AT+CPIN?" + nl)
+
+time.sleep(0.1)
+output = port.readlines()
+ready = p.search(output)
+
+if not ready:
+    port.write(b"AT+CPIN=2401" + nl)
+    time.sleep(0.5)
+    port.write(b"AT+CPIN?" + nl)
+    if not ready:
+        raise RuntimeError("sim is not unlocked")
+
+time.sleep(0.1)
 
 while True:
         msg = port.readlines()
