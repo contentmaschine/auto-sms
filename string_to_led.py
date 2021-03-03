2facda67a94bdfd315cd9e4991155c50ca6be081
felix <felix.weidenholzer@outlook.com>
2021-03-04 00:28:43 +0100

    trying KeyboardInterrupt except

--- string_to_led.py
+++ string_to_led.py
@@ -4,33 +4,37 @@ import time
 
 
 def string_to_led(message: str, pin_number: int):
-    gpio.setmode(gpio.BCM)
-    gpio.setwarnings(False)
-    gpio.setup(pin_number, gpio.OUT)
+    try:
+        gpio.setmode(gpio.BCM)
+        gpio.setwarnings(False)
+        gpio.setup(pin_number, gpio.OUT)
 
-    morse_code = mt.encrypt(message)
+        morse_code = mt.encrypt(message)
 
-    time_word = 2
-    time_dot = 0.2
-    time_dash = time_dot * 3
-    time_space = 0.8
-    time_between = 0.2
+        time_word = 2
+        time_dot = 0.2
+        time_dash = time_dot * 3
+        time_space = 0.8
+        time_between = 0.2
 
-    while True:
-        for symbol in morse_code:
-            if symbol == ".":
-                gpio.output(pin_number, gpio.HIGH)
-                time.sleep(time_dot)
-                gpio.output(pin_number, gpio.LOW)
-                time.sleep(time_between)
+        while True:
+            for symbol in morse_code:
+                if symbol == ".":
+                    gpio.output(pin_number, gpio.HIGH)
+                    time.sleep(time_dot)
+                    gpio.output(pin_number, gpio.LOW)
+                    time.sleep(time_between)
 
-            if symbol == "-":
-                gpio.output(pin_number, gpio.HIGH)
-                time.sleep(time_dash)
-                gpio.output(pin_number, gpio.LOW)
-                time.sleep(time_between)
+                if symbol == "-":
+                    gpio.output(pin_number, gpio.HIGH)
+                    time.sleep(time_dash)
+                    gpio.output(pin_number, gpio.LOW)
+                    time.sleep(time_between)
 
-            if symbol == " ":
-                time.sleep(time_space)
+                if symbol == " ":
+                    time.sleep(time_space)
 
-        time.sleep(time_word)
+            time.sleep(time_word)
+
+    except KeyboardInterrupt:
+        gpio.cleanup()
