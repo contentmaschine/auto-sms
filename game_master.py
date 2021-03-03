b3ffcd3601af222818384ccea926c20c5f84ab5b
felix <felix.weidenholzer@outlook.com>
2021-03-04 00:31:34 +0100

    nothing works, back to the start

--- game_master.py
+++ game_master.py
@@ -1,11 +1,7 @@
 from string_to_led import string_to_led
 import concurrent.futures
-import RPi.GPIO as gpio
 
-try:
-    executor = concurrent.futures.ThreadPoolExecutor(max_workers=3)
-    executor.submit(string_to_led, "Y", 6)
-    executor.submit(string_to_led, "A", 5)
-    executor.submit(string_to_led, "Y", 26)
-except KeyboardInterrupt:
-    gpio.cleanup()
+executor = concurrent.futures.ThreadPoolExecutor(max_workers=3)
+executor.submit(string_to_led, "Y", 6)
+executor.submit(string_to_led, "A", 5)
+executor.submit(string_to_led, "Y", 26)
