c4e1a7d15fec367e2e577e20f4b212d96e54b03d
felix <felix.weidenholzer@outlook.com>
2021-03-04 00:18:15 +0100

    _

--- game_master.py
+++ game_master.py
@@ -2,6 +2,6 @@ from string_to_led import string_to_led
 import concurrent.futures
 
 executor = concurrent.futures.ThreadPoolExecutor(max_workers=3)
-executor.submit(string_to_led, "Y", 5)
-executor.submit(string_to_led, "A", 6)
+executor.submit(string_to_led, "Y", 6)
+executor.submit(string_to_led, "A", 5)
 executor.submit(string_to_led, "Y", 26)
