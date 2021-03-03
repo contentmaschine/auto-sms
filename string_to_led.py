9fc729c37da45ac420359af583fd204b154e46da
felix <felix.weidenholzer@outlook.com>
2021-03-04 00:01:35 +0100

    threading works now, try and finally outside of function didn't work, not trying inside

--- string_to_led.py
+++ string_to_led.py
@@ -8,42 +8,44 @@ import concurrent.futures as cf
 
 
 def string_to_led(message: str, pin_number: int):
-    gpio.setmode(gpio.BCM)
-    gpio.setwarnings(False)
-    gpio.setup(pin_number, gpio.OUT)
+    try:
+        gpio.setmode(gpio.BCM)
+        gpio.setwarnings(False)
+        gpio.setup(pin_number, gpio.OUT)
 
-    morse_code = mt.encrypt(message)
+        morse_code = mt.encrypt(message)
 
-    time_word = 2
-    time_dot = 0.2
-    time_dash = time_dot * 3
-    time_space = 0.8
-    time_between = 0.2
+        time_word = 2
+        time_dot = 0.2
+        time_dash = time_dot * 3
+        time_space = 0.8
+        time_between = 0.2
 
-    while True:
-        for symbol in morse_code:
-            if symbol == ".":
-                gpio.output(pin_number, gpio.HIGH)
-                time.sleep(time_dot)
-                gpio.output(pin_number, gpio.LOW)
-                time.sleep(time_between)
+        while True:
+            for symbol in morse_code:
+                if symbol == ".":
+                    gpio.output(pin_number, gpio.HIGH)
+                    time.sleep(time_dot)
+                    gpio.output(pin_number, gpio.LOW)
+                    time.sleep(time_between)
 
-            if symbol == "-":
-                gpio.output(pin_number, gpio.HIGH)
-                time.sleep(time_dash)
-                gpio.output(pin_number, gpio.LOW)
-                time.sleep(time_between)
+                if symbol == "-":
+                    gpio.output(pin_number, gpio.HIGH)
+                    time.sleep(time_dash)
+                    gpio.output(pin_number, gpio.LOW)
+                    time.sleep(time_between)
 
-            if symbol == " ":
-                time.sleep(time_space)
+                if symbol == " ":
+                    time.sleep(time_space)
 
-        time.sleep(time_word)
+            time.sleep(time_word)
+
+    finally:
+        gpio.cleanup()
 
 
-try:
-    executor = cf.ThreadPoolExecutor(max_workers=3)
-    a = executor.submit(string_to_led, "SOS", 5)
-    b = executor.submit(string_to_led, "ATA", 6)
 
-finally:
-    gpio.cleanup()
+executor = cf.ThreadPoolExecutor(max_workers=3)
+executor.submit(string_to_led, "WE", 5)
+executor.submit(string_to_led, "DO", 6)
+executor.submit(string_to_led, "IT", 26)
