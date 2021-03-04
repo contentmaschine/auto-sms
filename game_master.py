1922c87460b844d5db34eff415ec22d2c8df4506
felix <felix.weidenholzer@outlook.com>
2021-03-04 23:29:19 +0100

    game master start_morse function

--- game_master.py
+++ game_master.py
@@ -1,7 +1,10 @@
 from string_to_led import string_to_led
 import concurrent.futures
 
-executor = concurrent.futures.ThreadPoolExecutor(max_workers=3)
-executor.submit(string_to_led, "Y", 6)
-executor.submit(string_to_led, "A", 5)
-executor.submit(string_to_led, "Y", 26)
+
+def start_morse(words_and_pins: dict):
+    n = len(words_and_pins)
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=n)
+    for word in words_and_pins:
+        pin = words_and_pins.get(word)
+        executor.submit(string_to_led, word, pin)
