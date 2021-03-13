c5bd35fb0a84b8357a2685000c64d3ebdcfb5f71
felix <felix.weidenholzer@outlook.com>
2021-03-13 22:19:52 +0100

    wire 1.0

--- wires.py
+++ wires.py
@@ -1,15 +1,17 @@
 import gpiozero
 import time
 
-#test2
 
-correct_pin = 17
+cuttable_wires_pin = 17
+do_not_cut_wires_pin = 27
 
-button = gpiozero.Button(correct_pin)
+cuttable_wires_still_connected = gpiozero.Button(cuttable_wires_pin).is_pressed
+do_not_cut_wires_still_connected = gpiozero.Button(do_not_cut_wires_pin).is_pressed
 
 while True:
-    if button.is_pressed:
-        print("connected")
-    else:
-        print("not connected")
-    time.sleep(1)
+    if not cuttable_wires_still_connected:
+        print("success")
+        pass
+    if not do_not_cut_wires_still_connected:
+        print("STRIKE")
+    time.sleep(0.1)
