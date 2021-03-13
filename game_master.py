6b8a561c22b57c7516c9fc4fba18360e79b224f3
felix <felix.weidenholzer@outlook.com>
2021-03-13 23:52:23 +0100

    just switched words and pins welp

--- game_master.py
+++ game_master.py
@@ -2,15 +2,10 @@ from string_to_led import string_to_led
 import concurrent.futures
 
 
-#the pins are 5, 6, 26 from left to right
-def start_morse(words_and_pins: dict):
-    n = 3
+# the pins are 5, 6, 26 from left to right
+def start_morse(pins_and_words: dict):
+    n = len(pins_and_words)
     executor = concurrent.futures.ThreadPoolExecutor(max_workers=n)
-    for word in words_and_pins:
-        pin = words_and_pins.get(word)
-        # to handle equal keys, which don't get recognised outside of a list
-        if isinstance(pin, list):
-            for element in pin:
-                executor.submit(string_to_led, word, element)
-        else:
-            executor.submit(string_to_led, word, pin)
+    for pin in pins_and_words:
+        word = pins_and_words.get(pin)
+        executor.submit(string_to_led, word, pin)
