1a17d33b62b26dadc1a73a3bc14f9700a3a5daf7
felix <felix.weidenholzer@outlook.com>
2021-03-13 22:47:08 +0100

    whatever

--- wires.py
+++ wires.py
@@ -2,8 +2,8 @@ import gpiozero
 import time
 
 
-cuttable_wires_pin = 17
-do_not_cut_wires_pin = 27
+cuttable_wires_pin = 27
+do_not_cut_wires_pin = 17
 
 cuttable_wires_still_connected = gpiozero.Button(cuttable_wires_pin).is_pressed
 do_not_cut_wires_still_connected = gpiozero.Button(do_not_cut_wires_pin).is_pressed
