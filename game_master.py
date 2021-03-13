99f9b27b4ebacebc5f475d00c1fa99a461745555
felix <felix.weidenholzer@outlook.com>
2021-03-13 23:44:59 +0100

    comment

--- game_master.py
+++ game_master.py
@@ -2,6 +2,7 @@ from string_to_led import string_to_led
 import concurrent.futures
 
 
+#the pins are 5, 6, 26 from left to right
 def start_morse(words_and_pins: dict):
     n = 3
     executor = concurrent.futures.ThreadPoolExecutor(max_workers=n)
