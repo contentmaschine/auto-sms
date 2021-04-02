d40c91d591f9e36de566359883002912afc1e175
felix <felix.weidenholzer@outlook.com>
2021-04-02 19:40:33 +0200

    trying out sth with scopes in wires

--- wires.py
+++ wires.py
@@ -1,5 +1,4 @@
-from gpiozero import Button, DigitalInputDevice
-import time
+from gpiozero import DigitalInputDevice
 import game_state
 
 
@@ -10,6 +9,12 @@ right_connection = DigitalInputDevice(pin=right_pin, pull_up=True)
 wrong_connection = DigitalInputDevice(pin=wrong_pin, pull_up=True)
 
 def wires():
+    right_pin = 27
+    wrong_pin = 17
+
+    right_connection = DigitalInputDevice(pin=right_pin, pull_up=True)
+    wrong_connection = DigitalInputDevice(pin=wrong_pin, pull_up=True)
+
     right_connection.when_deactivated = right_wire
     wrong_connection.when_deactivated = game_state.strike
 
