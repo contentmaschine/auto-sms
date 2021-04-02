310d410afe1d391ae5c28ca79d2378d1d2cb4f32
felix <felix.weidenholzer@outlook.com>
2021-04-02 19:43:15 +0200

    not scopes, but script ending

--- wires.py
+++ wires.py
@@ -9,14 +9,11 @@ right_connection = DigitalInputDevice(pin=right_pin, pull_up=True)
 wrong_connection = DigitalInputDevice(pin=wrong_pin, pull_up=True)
 
 def wires():
-    right_pin = 27
-    wrong_pin = 17
-
-    right_connection = DigitalInputDevice(pin=right_pin, pull_up=True)
-    wrong_connection = DigitalInputDevice(pin=wrong_pin, pull_up=True)
-
     right_connection.when_deactivated = right_wire
     wrong_connection.when_deactivated = game_state.strike
+
+    while not game_state.wires_done:
+        pass
 
 def right_wire():
     game_state.success(16)
