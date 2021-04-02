159ed3ac7aabb690adb6ab211758770519987f04
felix <felix.weidenholzer@outlook.com>
2021-04-02 12:18:03 +0200

    now threaded strike leds

--- game_state.py
+++ game_state.py
@@ -1,5 +1,6 @@
 from gpiozero import LED
 from signal import pause
+import concurrent.futures
 
 
 strike_counter = 0
@@ -8,14 +9,19 @@ strike_counter = 0
 def strike(strike_pins=(23, 24, 25)):
     global strike_counter
     active_pin = strike_pins[strike_counter]
-    strike_led = LED(active_pin)
-    strike_led.on()
+
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+    executor.submit(strike_led_on, active_pin)
 
     strike_counter += 1
     if strike_counter > 2:
         # explode
         raise AttributeError("you exploded")
-    #return strike_counter
+
+
+def strike_led_on(active_pin):
+    strike_led = LED(active_pin)
+    strike_led.on()
     pause()
 
 
