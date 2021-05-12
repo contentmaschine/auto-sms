74b29aa6c3c3c6792ec020f0f39f35792fc751fc
felix <felix.weidenholzer@outlook.com>
2021-05-12 22:15:39 +0200

    changed back to only first

--- game_master.py
+++ game_master.py
@@ -15,16 +15,17 @@ game_state.start_game()
 
 with concurrent.futures.ProcessPoolExecutor() as executor:
     executor.submit(lcd_assets.countdown, 5, 0)
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    wires = executor.submit(wires.wires)
-    simon_says = executor.submit(simon_says.simon_says)
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        wires = executor.submit(wires.wires)
+        simon_says = executor.submit(simon_says.simon_says)
 
     # both threads return True, when done correctly
-    if wires.result() and simon_says.result():
-        # artistic pause
-        time.sleep(1)
-        with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-            executor.submit(morse.morse, {5: "SMS", 6: "FELIX", 26: "DEFUSE"})
-            sms_reader = executor.submit(sms_reader.sms_reader)
-            if sms_reader.result():
-                lcd_assets.win_screen()
+        if wires.result() and simon_says.result():
+            # artistic pause
+            time.sleep(1)
+            with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+                executor.submit(morse.morse, {5: "SMS", 6: "FELIX", 26: "DEFUSE"})
+                sms_reader = executor.submit(sms_reader.sms_reader)
+                if sms_reader.result():
+                    lcd_assets.win_screen()
