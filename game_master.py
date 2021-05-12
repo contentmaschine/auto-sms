74ae75cd5a3e11e5acab9c60c92154e33e7150fb
felix <felix.weidenholzer@outlook.com>
2021-05-12 22:15:02 +0200

    changed back to only first

--- game_master.py
+++ game_master.py
@@ -13,8 +13,9 @@ lcd_assets.start_screen()
 # waits for red and blue button to be pushed
 game_state.start_game()
 
-with concurrent.futures.ProcessPoolExecutor(max_workers=3) as executor:
+with concurrent.futures.ProcessPoolExecutor() as executor:
     executor.submit(lcd_assets.countdown, 5, 0)
+with concurrent.futures.ThreadPoolExecutor() as executor:
     wires = executor.submit(wires.wires)
     simon_says = executor.submit(simon_says.simon_says)
 
